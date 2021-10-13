287ecf094b2b36bb6ada776ce080719131a98dd4
Marko Tasic <mtasic85@gmail.com>
2021-10-13 22:04:07 +0200

    initial example hello.js works

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # quickjs-fltk
+
 QuickJS FLTK
+
+## Build
+```bash
+git clone git@github.com:mtasic85/quickjs-fltk.git
+cd quickjs-fltk
+git submodule update --init --recursive
+```
---
+++ hello.js
@@ -0,0 +1,34 @@
+import { CFunction, CCallback } from './quickjs-ffi.js';
+
+const LIBCFLTK = './libcfltk.so.1.2.5'
+
+const Fl_init_all = new CFunction(LIBCFLTK, 'Fl_init_all', null, 'void');
+const Fl_register_images = new CFunction(LIBCFLTK, 'Fl_register_images', null, 'void');
+const Fl_lock = new CFunction(LIBCFLTK, 'Fl_lock', null, 'int');
+const Fl_Window_new = new CFunction(LIBCFLTK, 'Fl_Window_new', null, 'pointer', 'int', 'int', 'int', 'int', 'string');
+const Fl_Button_new = new CFunction(LIBCFLTK, 'Fl_Button_new', null, 'pointer', 'int', 'int', 'int', 'int', 'string');
+const Fl_Window_end = new CFunction(LIBCFLTK, 'Fl_Window_end', null, 'void', 'pointer');
+const Fl_Window_show = new CFunction(LIBCFLTK, 'Fl_Window_show', null, 'void', 'pointer');
+const Fl_Widget_set_label = new CFunction(LIBCFLTK, 'Fl_Widget_set_label', null, 'void', 'pointer', 'string');
+const Fl_Window_set_label = new CFunction(LIBCFLTK, 'Fl_Window_set_label', null, 'void', 'pointer', 'string');
+const Fl_Button_set_callback = new CFunction(LIBCFLTK, 'Fl_Button_set_callback', null, 'void', 'pointer', 'pointer', 'pointer');
+const Fl_run = new CFunction(LIBCFLTK, 'Fl_run', null, 'int');
+
+let cb = new CCallback(function cb(widget, win) {
+    Fl_Widget_set_label.invoke(widget, "Works!");
+    Fl_Window_set_label.invoke(win, "Hello world!");
+}, null, 'void', 'pointer', 'pointer').cfuncptr;
+
+function main() {
+    Fl_init_all.invoke();        // init all styles
+    Fl_register_images.invoke(); // necessary for image support
+    Fl_lock.invoke();            // necessary for multithreaded support
+    let w = Fl_Window_new.invoke(100, 100, 400, 300, "Hello ...");
+    let b = Fl_Button_new.invoke(160, 210, 80, 40, "Click me");
+    Fl_Window_end.invoke(w);
+    Fl_Window_show.invoke(w);
+    Fl_Button_set_callback.invoke(b, cb, w);
+    return Fl_run.invoke();
+}
+
+main();
