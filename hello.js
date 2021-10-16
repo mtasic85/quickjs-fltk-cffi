16b5dd573c0588a0f398c9587839fff3976656e8
Marko Tasic <mtasic85@gmail.com>
2021-10-16 19:53:50 +0200

    cparser-quickjs-ffi: autogen for fltk, _get_func_decl_params_types works

--- hello.js
+++ hello.js
@@ -1,33 +1,68 @@
 import { CFunction, CCallback } from './quickjs-ffi.js';
 
-const LIBCFLTK = './libcfltk.so.1.2.5'
+const _quickjs_ffi_wrap_ptr_func_decl = (lib, name, nargs, ...types) => {
+    // wrap C function
+    const c_types = types.map(type => {
+        if (typeof type == 'string') {
+            return type;
+        } else if (typeof type == 'object') {
+            if (type.type == 'PtrFuncDecl') {
+                return 'pointer';
+            } else {
+                throw new Error('Unsupported type');
+            }
+        } else {
+            throw new Error('Unsupported type');
+        }
+    });
 
-const _Fl_init_all = new CFunction(LIBCFLTK, 'Fl_init_all', null, 'void');
-const Fl_init_all = () => _Fl_init_all.invoke();
+    const c_func = new CFunction(lib, name, nargs, ...c_types);
+    
+    const js_func = (...js_args) => {
+        const c_args = types.slice(1).map((type, i) => {
+            const js_arg = js_args[i];
 
-const _Fl_register_images = new CFunction(LIBCFLTK, 'Fl_register_images', null, 'void');
-const Fl_register_images = () => _Fl_register_images.invoke();
+            if (typeof type == 'string') {
+                return js_arg;
+            } else if (typeof type == 'object') {
+                if (type.type == 'PtrFuncDecl') {
+                    const c_cb = new CCallback(js_arg, null, ...type.types);
+                    return c_cb.cfuncptr;
+                } else {
+                    throw new Error('Unsupported type');
+                }
+            } else {
+                throw new Error('Unsupported type');
+            }
+        });
 
-const _Fl_lock = new CFunction(LIBCFLTK, 'Fl_lock', null, 'int');
-const Fl_lock = () => _Fl_lock.invoke();
+        return c_func.invoke(...c_args);
+    };
 
-const _Fl_Window_new = new CFunction(LIBCFLTK, 'Fl_Window_new', null, 'pointer', 'int', 'int', 'int', 'int', 'string');
-const Fl_Window_new = (...args) => _Fl_Window_new.invoke(...args);
+    return js_func;
+};
 
-const _Fl_Button_new = new CFunction(LIBCFLTK, 'Fl_Button_new', null, 'pointer', 'int', 'int', 'int', 'int', 'string');
-const Fl_Button_new = (...args) => _Fl_Button_new.invoke(...args);
+const LIBCFLTK = './libcfltk.so.1.2.5';
 
-const Fl_Window_end = new CFunction(LIBCFLTK, 'Fl_Window_end', null, 'void', 'pointer');
-const Fl_Window_show = new CFunction(LIBCFLTK, 'Fl_Window_show', null, 'void', 'pointer');
-const Fl_Widget_set_label = new CFunction(LIBCFLTK, 'Fl_Widget_set_label', null, 'void', 'pointer', 'string');
-const Fl_Window_set_label = new CFunction(LIBCFLTK, 'Fl_Window_set_label', null, 'void', 'pointer', 'string');
-const Fl_Button_set_callback = new CFunction(LIBCFLTK, 'Fl_Button_set_callback', null, 'void', 'pointer', 'pointer', 'pointer');
-const Fl_run = new CFunction(LIBCFLTK, 'Fl_run', null, 'int');
+const Fl_init_all = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_init_all', null, 'void');
+const Fl_register_images = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_register_images', null, 'void')
+const Fl_lock = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_lock', null, 'int');
+const Fl_Window_new = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_Window_new', null, 'pointer', 'int', 'int', 'int', 'int', 'string');
+const Fl_Button_new = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_Button_new', null, 'pointer', 'int', 'int', 'int', 'int', 'string');
+const Fl_Window_end = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_Window_end', null, 'void', 'pointer');
+const Fl_Window_show = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_Window_show', null, 'void', 'pointer');
+const Fl_Widget_set_label = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_Widget_set_label', null, 'void', 'pointer', 'string');
+const Fl_Window_set_label = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_Window_set_label', null, 'void', 'pointer', 'string');
+const Fl_run = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_run', null, 'int');
 
-let cb = new CCallback(function cb(widget, win) {
-    Fl_Widget_set_label.invoke(widget, "Works!");
-    Fl_Window_set_label.invoke(win, "Hello world!");
-}, null, 'void', 'pointer', 'pointer').cfuncptr;
+const Fl_Button_set_callback = _quickjs_ffi_wrap_ptr_func_decl(LIBCFLTK, 'Fl_Button_set_callback', null,
+    'void', 'pointer', {'type': 'PtrFuncDecl', 'types': ['void', 'pointer', 'pointer']}, 'pointer'
+);
+
+function cb(widget, win) {
+    Fl_Widget_set_label(widget, "Works!");
+    Fl_Window_set_label(win, "Hello world!");
+}
 
 function main() {
     Fl_init_all();        // init all styles
@@ -35,10 +70,10 @@ function main() {
     Fl_lock();            // necessary for multithreaded support
     let w = Fl_Window_new(100, 100, 400, 300, "Hello ...");
     let b = Fl_Button_new(160, 210, 80, 40, "Click me");
-    Fl_Window_end.invoke(w);
-    Fl_Window_show.invoke(w);
-    Fl_Button_set_callback.invoke(b, cb, w);
-    return Fl_run.invoke();
+    Fl_Window_end(w);
+    Fl_Window_show(w);
+    Fl_Button_set_callback(b, cb, w);
+    return Fl_run();
 }
 
 main();
